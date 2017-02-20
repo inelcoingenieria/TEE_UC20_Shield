4f1ab77decb7e0ad80d278906c8c090c09ffff52
ThaiEasyElec <support@thaieasyelec.com>
2017-02-20 19:16:04 +0700

    Update Lib & Example Anto.io
    Update Lib & Example for Anto.io

--- uc_anto.cpp
+++ uc_anto.cpp
@@ -1,15 +1,85 @@
 #include "uc_anto.h"
+#include "uc_mqtt.h"
 
+UCxMQTT mqtt;
 
 
 UCxANTO::anto()
 {
+	//antocallback = func_null;
 	//AntoIO::AntoIO(const char *user, const char *token, const char *thing, const char *client_id): 
     
 	//mqtt(client_id, ANTO_BROKER, ANTO_BROKER_PORT),_user(user), _token(token), _thing(thing)
 }
 
+void  callback_(String topic ,char *playload,unsigned char length)
+{
+	Serial.println();
+  Serial.println(F("%%%%%%%%%%%%%%%%%%%%%%%%%%%%"));
+  Serial.print(F("Topic --> "));
+  Serial.println(topic);
+  playload[length]=0;
+  String str_data(playload);
+  Serial.print(F("Playload --> "));
+  Serial.println(str_data);
+}
 
+void UCxANTO :: begin(String username,String password,String name)
+{
+	anto_USER = username;
+	anto_PASS = password;
+	anto_Thing = name;
+	mqtt.callback = callback_;
+}
+
+bool UCxANTO :: connectServer()
+{
+	
+	do
+	{
+		gsm.debug(F("Connect Server"));
+		gsm.debug(F("wait connect"));
+		if(mqtt.DisconnectMQTTServer())
+		{
+			mqtt.ConnectMQTTServer(ANTO_SERVER ,ANTO_PORT);
+		}
+		delay(500);
+	}
+	while(!mqtt.ConnectState());
+	gsm.debug(F("Server Connected"));
+	unsigned char ret = mqtt.Connect(anto_Thing,anto_USER,anto_PASS);
+	Serial.println(mqtt.ConnectReturnCode(ret));
+		if(ret==0)
+		{
+			
+			return(true);
+		}
+		return(false);
+}
+
+
+void UCxANTO :: pub(String channel,String msg)
+{
+	String dat = "channel/"+anto_USER+"/"+anto_Thing+"/"+channel;
+	mqtt.Publish(dat,msg);
+}
+void UCxANTO :: pub(String channel,int msg)
+{
+	String dat = "channel/"+anto_USER+"/"+anto_Thing+"/"+channel;
+	mqtt.Publish(dat,String(msg));
+}
+void UCxANTO :: sub(String channel)
+{
+	String dat = "channel/"+anto_USER+"/"+anto_Thing+"/"+channel;
+	mqtt.Subscribe(dat);
+}
+bool UCxANTO :: loop()
+{
+	mqtt.MqttLoop();
+	if(!mqtt.ConnectState())
+     return(true);
+	return(false);
+}
 
 
 
--- uc_anto.h
+++ uc_anto.h
@@ -3,10 +3,31 @@
 
 #include "TEE_UC20.h"
 
+#define ANTO_SERVER      "service.anto.io"
+#define ANTO_PORT        "1883"
+#define ANTO_ID          "3G"
+
+//#define MQTT_USER        "settakan"
+//#define MQTT_PASSWORD    "HZaAaDn4smPefzlwXcvbRrlDkYZZAaMWtO4jh6q4"
+
+
+
 class UCxANTO
 {
 	public:
 	anto();
+	void begin(String username,String password,String name);
+	bool connectServer();
+	void pub(String channel,String msg);
+	void pub(String channel,int msg);
+	void sub(String channel);
+	bool loop();
+	//void callback_(String topic ,char *playload,unsigned char length);
+	
+	private:
+	String anto_USER;
+	String anto_PASS;
+	String anto_Thing;
 
 };
 
