c30193cb2115cc3e3b86fd23f6747a85395650f5
ThaiEasyElec <support@thaieasyelec.com>
2020-05-11 13:32:31 +0700

    Update GNSS.ino

--- examples/GNSS/GNSS/GNSS.ino
+++ examples/GNSS/GNSS/GNSS.ino
@@ -14,6 +14,24 @@ void data_out(char data)
 {
   Serial.write(data);
 }
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 void setup() 
 {
   Serial.begin(9600);
@@ -28,10 +46,16 @@ void setup()
   Serial.println(gsm.SignalQuality());
   gps.Start();
   Serial.print(F("GPS Start"));
+  delay(500);
 }
 
 void loop() 
 {
-  Serial.println(gps.GetPosition());
+  String GPS_DATA = gps.GetPosition();
+  Serial.println(GPS_DATA);
+  String latitude = getValue(GPS_DATA,',',1 );
+  Serial.println("latitude : " + latitude);
+  String longitude = getValue(GPS_DATA,',',2 );
+  Serial.println("longitude : " + longitude);
   delay(500);   
 }
